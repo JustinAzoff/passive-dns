382a423b2932d75ba1c2ef54b7d9a772ae43b7ae
Justin Azoff <justin@bouncybouncy.net>
2008-06-11 20:45:15 +0000

    capture and process

--- dns_capture.sh
+++ dns_capture.sh
@@ -5,8 +5,6 @@ fn=dns_$(date +"%Y-%m-%d_%H:%M").pcap
 
 D=$(date +"%Y/%m/%d")
 mkdir -p ${LOC}/${D}
-ot=${LOC}/${D}/capturing_$fn
 o=${LOC}/${D}/$fn
 echo $o
-timeout -2 300 tcpdump -n -i sniff1 'udp port 53 and ( udp[10] & 0x04 != 0 )' -w $ot -s 0
-mv $ot $o
+tcpdump -n -i sniff1 'udp port 53 and ( udp[10] & 0x04 != 0 )' -w $o -s 0 -C 10
---
+++ dns_process.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+import os
+import time
+
+LOC="/var/captures/dns"
+SIZE_TIMEOUT = 5
+
+def next_filename(f):
+    if not f[-1].isdigit():
+        return f + '1'
+
+    fn, number = f.split(".pcap")
+    new_number = int(number) + 1
+    return "%s.pcap%d" % ( fn, new_number)
+
+def is_growing(f):
+    size = os.stat(f).st_size
+    time.sleep(0.1)
+    for x in range(SIZE_TIMEOUT):
+        time.sleep(1)
+        newsize = os.stat(f).st_size
+        if newsize != size:
+            return True
+    return False
+
+def find(d):
+    for d, path, files in os.walk(d):
+        for f in files:
+            fn = os.path.join(d, f)
+            yield(fn)
+
+def process(fn):
+    outfn = fn.replace("pcap","") + ".txt.gz"
+    print 'process', fn
+    newfn = fn + ".processing"
+    os.rename(fn, newfn)
+
+    #not sure about leaks, so lets just exec this
+    ret = os.system("dns_parse_pcap.py %s %s" % (newfn, outfn))
+    if ret != 0:
+        raise Exception("Error running dns_parse_pcap %s" % newfn)
+    os.unlink(newfn)
+
+
+def main():
+    for f in find(LOC):
+        if 'processing' in f: continue
+
+        if os.path.exists(next_filename(f)) or not is_growing(f):
+            process(f)
+
+if __name__ == "__main__":
+    main()
