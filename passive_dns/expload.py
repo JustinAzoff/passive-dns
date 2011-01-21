ddeb1f36dad21b7653d3331e370e32a818eb5b85
Justin Azoff <justin@bouncybouncy.net>
2011-01-20 20:37:15 -0500

    change expload to be iterative, yield both records and 'new' keys

--- passive_dns/expload.py
+++ passive_dns/expload.py
@@ -6,33 +6,34 @@ c = SearchClient()
 
 
 
-def expload(qs, seen=None):
-    if seen is None:
-        seen = set()
-    seen.update(qs)
-    new = []
-    
-    for q in qs:
-        q_results = c.search_question(" " + q)
-        a_results = c.search_answer(q + " ")
-        results = q_results + a_results
-        for x in results:
-            yield x
+def expload(qs):
+    seen = set()
+    todo = qs
+    while todo:
+        new = []
+        seen.update(todo)
+        for q in todo:
+            q_results = c.search_question(" " + q)
+            a_results = c.search_answer(q + " ")
+            results = q_results + a_results
+            for x in results:
+                yield x
 
-            if x['key'] not in seen:
-                new.append(x['key'])
-            if x['value'] not in seen:
-                new.append(x['value'])
-    if not new:
-        return
-
-    for x in expload(new, seen):
-        yield x
+                if x['key'] not in seen:
+                    new.append(x['key'])
+                if x['value'] not in seen:
+                    new.append(x['value'])
+        todo = new
+        if new:
+            yield {"new": new}
 
 def main():
     qs = sys.argv[1:]
     for x in expload(qs):
-        show(x)
+        if 'new' in x:
+            print "\nnew:", ' '.join(x['new'])
+        else :
+            show(x)
 
 if __name__ == "__main__":
     main()
