9f950e00cf937d4eeba331efea8f38d4fd21dd67
Justin Azoff <justin@bouncybouncy.net>
2011-01-20 20:58:26 -0500

    some logic fixes, filter akamai

--- passive_dns/expload.py
+++ passive_dns/expload.py
@@ -11,18 +11,23 @@ def expload(qs):
     todo = qs
     while todo:
         new = []
-        seen.update(todo)
         for q in todo:
             q_results = c.search_question(" " + q)
             a_results = c.search_answer(q + " ")
             results = q_results + a_results
             for x in results:
-                yield x
-
-                if x['key'] not in seen:
-                    new.append(x['key'])
-                if x['value'] not in seen:
-                    new.append(x['value'])
+                k = x['key']
+                v = x['value']
+                if k not in seen or v not in seen:
+                    yield x
+                    if k not in seen:
+                        new.append(k)
+                        seen.add(k)
+                    if v not in seen:
+                        new.append(v)
+                        seen.add(v)
+        #filter akamai, otherwise this goes on forever
+        new = [x for x in new if not x.endswith("akamai.net")]
         todo = new
         if new:
             yield {"new": new}
@@ -31,9 +36,9 @@ def main():
     qs = sys.argv[1:]
     for x in expload(qs):
         if 'new' in x:
-            print "\nnew:", ' '.join(x['new'])
+            print "new:", ' '.join(x['new'])
+            print
         else :
             show(x)
-
 if __name__ == "__main__":
     main()
