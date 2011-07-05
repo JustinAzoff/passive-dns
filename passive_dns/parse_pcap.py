db0e9f51b0d504c37e8c8fc87a73681bfcf1e6d8
Justin Azoff <justin@bouncybouncy.net>
2011-07-05 14:46:03 -0400

    scapy was nice, but too slow. revert to pcapc+dnspython

--- passive_dns/parse_pcap.py
+++ passive_dns/parse_pcap.py
@@ -2,40 +2,64 @@
 """Extract from a pcap file of all dns queries:
 time, query, answer, type, ttl
 """
+import pcapy
+import dns.message
 import time, datetime
-from scapy.all import sniff, IP, DNS, DNSQR, DNSRR
+import gzip
+try:
+    import psyco
+    psyco.full()
+except:
+    pass
+
+OFFSET = 42
+A = 1
+CNAME = 5
+
+TYPES = {
+    A: 'A',
+    CNAME: 'CNAME'
+}
 
 def date(d):
     return datetime.datetime.fromtimestamp(d).strftime("%Y-%m-%d %H:%M:%S")
 
-def payloads(x):
-    yield x
-    while True:
-        x = x.payload
-        if x:
-            yield x
-        else:
-            break
+def get_answers(m):
+    for a in m.answer:
+        if a.rdtype not in TYPES: continue
+        for i in a:
+            yield i.to_text().lower(), TYPES[a.rdtype], a.ttl
+#            if a.rdtype == CNAME:
+                #raise 'test'
 
-wanted_types = ['A', 'CNAME']
-
+def get_query(m):
+    try :
+        query = m.question[0].to_text().split()[0]
+    except IndexError:
+        return None
+    if query.endswith("."):
+        query = query[:-1]
+    return query.lower()
+        
 class Statmaker:
     def __init__(self):
         self.ipnames = {}
 
-    def __call__(self, pkt):
-        ipn = self.ipnames
-        if DNSRR not in pkt:
+    def __call__(self, header, data):
+
+        ts, _ =  header.getts()
+        try :
+            m = dns.message.from_wire(data[OFFSET:])
+        except:
             return
-        ts = pkt.time
-        dnsrr = pkt[DNSRR]
-        query = dnsrr.rrname
+        query = get_query(m)
+        if not query:
+            return
 
-        resp = [(r.rdata, r.sprintf("%type%"), r.ttl) for r in payloads(dnsrr)]
+        ipn = self.ipnames
 
-        for answer, type, ttl in resp:
+        for answer, type, ttl in get_answers(m):
             tup = (answer, query, type)
-            if type not in wanted_types: continue
             if tup in ipn:
                 r = ipn[tup]
                 r.update({'last': ts, 'ttl': ttl})
@@ -47,7 +71,8 @@ def parse(fn):
     """Process a pcap file and return a dictionary of
         (answer, query, type) -> {first, last, ttl}"""
     s = Statmaker()
-    sniff(offline=fn, prn=s, store=0)
+    pcap = pcapy.open_offline(fn)
+    pcap.loop(0, s)
 
     return s
 
