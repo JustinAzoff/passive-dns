4a5f739b5802dfaf4670dd13381b278e2481b34f
Justin Azoff <justin@bouncybouncy.net>
2011-07-05 14:28:47 -0400

    port to scapy

--- passive_dns/parse_pcap.py
+++ passive_dns/parse_pcap.py
@@ -2,63 +2,36 @@
 """Extract from a pcap file of all dns queries:
 time, query, answer, type, ttl
 """
-import pcapy
-import dns.message
 import time, datetime
-import gzip
-try:
-    import psyco
-    psyco.full()
-except:
-    pass
-
-OFFSET = 42
-A = 1
-CNAME = 5
-
-TYPES = {
-    A: 'A',
-    CNAME: 'CNAME'
-}
+from scapy.all import sniff, IP, DNS, DNSQR, DNSRR
 
 def date(d):
     return datetime.datetime.fromtimestamp(d).strftime("%Y-%m-%d %H:%M:%S")
 
-def get_answers(m):
-    for a in m.answer:
-        if a.rdtype not in TYPES: continue
-        for i in a:
-            yield i.to_text().lower(), TYPES[a.rdtype], a.ttl
-#            if a.rdtype == CNAME:
-                #raise 'test'
+def payloads(x):
+    yield x
+    while True:
+        x = x.payload
+        if x:
+            yield x
+        else:
+            break
 
-def get_query(m):
-    try :
-        query = m.question[0].to_text().split()[0]
-    except IndexError:
-        return None
-    if query.endswith("."):
-        query = query[:-1]
-    return query.lower()
-        
 class Statmaker:
     def __init__(self):
         self.ipnames = {}
 
-    def __call__(self, header, data):
-
-        ts, _ =  header.getts()
-        try :
-            m = dns.message.from_wire(data[OFFSET:])
-        except:
-            return
-        query = get_query(m)
-        if not query:
-            return
-
+    def __call__(self, pkt):
         ipn = self.ipnames
+        if DNSRR not in pkt:
+            return
+        ts = pkt.time
+        dnsrr = pkt[DNSRR]
+        query = dnsrr.rrname
 
-        for answer, type, ttl in get_answers(m):
+        resp = [(r.rdata, r.sprintf("%type%"), r.ttl) for r in payloads(dnsrr)]
+
+        for answer, type, ttl in resp:
             tup = (answer, query, type)
             if tup in ipn:
                 r = ipn[tup]
@@ -71,8 +44,7 @@ def parse(fn):
     """Process a pcap file and return a dictionary of
         (answer, query, type) -> {first, last, ttl}"""
     s = Statmaker()
-    pcap = pcapy.open_offline(fn)
-    pcap.loop(0, s)
+    sniff(offline=fn, prn=s, store=0)
 
     return s
 
